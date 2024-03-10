9f8270cf99d1e43ec7c4027d6c2084989dafd7f8
Abrar Rahman <121895217+iamabrar14@users.noreply.github.com>
2024-03-10 15:19:20 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+#Return the count of a given substring from a string
+n=input("Enter the string : ")
+x=input("Which word do you want to search ? ")
+counting=n.count(x)
+print("It appears : ",counting)
